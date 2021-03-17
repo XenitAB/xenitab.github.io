0dbd7f6333a96c5f6ea1a753c0323f70c3302b44
Robert Hansson <robert.hansson@xenit.se>
2021-03-17 21:01:47 +0100

    Added fonts variables

---
+++ src/utils/fonts.js
@@ -0,0 +1,9 @@
+const fonts = {
+  fontBold: "MaisonNeue-Bold",
+  fontDemi: "MaisonNeue-Demi",
+  fontMedium: "MaisonNeue-Medium",
+  fontLight: "MaisonNeue-Light",
+  fontBook: "MaisonNeue-Book",
+};
+
+export default fonts;
