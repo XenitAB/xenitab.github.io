6be98d515c0738d2bec58b63b6f745e3e38fb722
Simon Gottschlag <simon.gottschlag@xenit.se>
2021-09-24 13:54:00 +0200

    update logic for external docs using full format in sidebar

--- sidebars.js
+++ sidebars.js
@@ -1,45 +1,57 @@
 module.exports = {
-  docs: [
+  "docs": [
     {
-      type: "doc",
-      id: "index",
+      "type": "doc",
+      "id": "index"
     },
     {
-      type: "category",
-      label: "Xenit Kubernetes Service",
-      items: [
+      "type": "category",
+      "label": "Xenit Kubernetes Service",
+      "items": [
         {
-          type: "doc",
-          id: "xks/index"
+          "type": "doc",
+          "id": "xks/index"
         },
         {
-          type: "category",
-          label: "Developer Guide",
-          items: [
+          "type": "category",
+          "label": "Developer Guide",
+          "items": [
             "xks/developer-guide/introduction",
             "xks/developer-guide/best-practices",
             "xks/developer-guide/cd",
             "xks/developer-guide/cloud-iam",
             "xks/developer-guide/flux",
-            "xks/developer-guide/networking",
             "xks/developer-guide/secrets-management",
-            "xks/developer-guide/security",
-          ],
+            "xks/developer-guide/cd",
+            "xks/developer-guide/flux",
+            "xks/developer-guide/best-practices"
+          ]
         },
         {
-          type: "category",
-          label: "Operator Guide",
-          items: [
+          "type": "category",
+          "label": "Operator Guide",
+          "items": [
             "xks/operator-guide/index",
             "xks/operator-guide/aws-azdo",
             "xks/operator-guide/azure-devops-agents",
             "xks/operator-guide/blast-radius",
             "xks/operator-guide/blue-green",
             "xks/operator-guide/eks",
-            "xks/operator-guide/operator-guide",
-          ],
-        },
-      ],
+            "xks/operator-guide/operator-guide"
+          ]
+        }
+      ]
     },
-  ],
-};
+    {
+      "type": "category",
+      "label": "Projects",
+      "items": [
+        {
+          "type": "category",
+          "label": "Terraform Modules",
+          "items": []
+        }
+      ]
+    }
+  ]
+}
